0fea3c4ee3b9c56f6d1ea260de6dd795bf8aa5f8
wade00754 <wade00754@gmail.com>
2024-07-28 01:05:22 +0800

    train with v2 model

--- train.py
+++ train.py
@@ -1,12 +1,13 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
-dataset = "..\datasets\project.v2i\data.yaml"
+model = YOLO(R"runs\detect\train2\weights\best.pt")
+# model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+dataset = "..\datasets\project.v5i\data.yaml"
 
 if __name__ == "__main__":
     # Use the model
-    model.train(data=dataset, epochs=50)  # train the model
+    model.train(data=dataset, epochs=200)  # train the model
     model.val(data=dataset, split="test")  # evaluate model performance on the test set
     # model.predict(source="images", save=True)  # predict on an image
     # model.export(format="onnx")  # export a model to ONNX format
