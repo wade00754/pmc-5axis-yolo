262bcf194a93e452d29bc3f0217d0c975fa35e16
wade00754 <wade00754@gmail.com>
2024-09-21 01:31:18 +0800

    implement hands_on_button functions & rename some files

---
+++ trainning/test_predict.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO(R"best.pt")  # load a pretrained model (recommended for training)
+dataset = "..\datasets\project.v6i\data.yaml"
+
+if __name__ == "__main__":
+    model.predict(source=R"images/1.jpg", save=True)  # predict on an image
+    # model.export(format="onnx")  # export a model to ONNX format
